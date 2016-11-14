f0d28ed3cc39538a6c415789408ef3f24ded959c
me <me@example.org>
2016-11-13 20:06:13 -0800

    Move some functions into this_is_json.php

--- test/test_client.rb
+++ test/test_client.rb
@@ -2,9 +2,9 @@ require 'httparty'
 
 @result = HTTParty.post('http://localhost/sprusage/report.php',
   :body => {
-    :field1 => 'value1',
-    :field2 => 'value2',
-    :field3 => 'value3',
+    :field1 => ARGV[2] || 'value1',
+    :field2 => ARGV[3] || 'value2',
+    :field3 => ARGV[4] || 'value3',
     :username => ARGV[0],
     :password => ARGV[1],
   }.to_json,
--- this_is_json.php
+++ this_is_json.php
@@ -9,4 +9,36 @@
   }
 
   $params = json_decode(file_get_contents("php://input"), true);
+
+  function reply($code, $msg) {
+    print json_encode([
+      'result' => $code,
+      'msg' => $msg,
+    ]);
+  }
+
+  function restrict_page_to_users($db, $params, $users) {
+    if(!isset($params['username']) || !isset($params['password'])) {
+      reply(401, 'Please specify a username/password!');
+      exit(0);
+    }
+    $username = mysqli_real_escape_string($db, $params['username']);
+    $password = mysqli_real_escape_string($db, $params['password']);
+
+    $result = mysqli_query($db, "SELECT * FROM `users` WHERE `username`='$username'");
+    if(!$result) {
+      reply(500, "Query error: " . mysqli_error($db));
+      exit(1);
+    }
+
+    $row = mysqli_fetch_assoc($result);
+    if(!$row) {
+      reply(401, 'No such user!');
+      exit(1);
+    }
+    if($password !== $row['password']) {
+      reply(401, 'Bad password!');
+      exit(1);
+    }
+  }
 ?>
